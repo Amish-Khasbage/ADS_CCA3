91619101c82445bb1ebe6312be8cee96ea301028
Amish Khasbage <amish.workspace@gmail.com>
2025-11-08 12:31:20 +0530

    Binary Search Tree : Insertion.c

---
+++ Binary Search Tree : Insertion.c
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  	
+  	void preOrder(Node *root) {
+		
+      	if( root == NULL )
+          	return;
+      
+      	std::cout << root->data << " ";
+      	
+      	preOrder(root->left);
+      	preOrder(root->right);
+    }
+
+Node* insert(Node* root, int data) {
+    if (root == NULL) {
+        Node* new_node = (Node*)malloc(sizeof(Node));
+        new_node->data = data;
+        new_node->left = NULL;
+        new_node->right = NULL;
+        return new_node;
+    }
+
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else {
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+};
