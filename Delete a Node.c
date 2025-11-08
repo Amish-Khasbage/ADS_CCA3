647bc18e166dcfe3e3c0b144970c902346f10c99
Amish Khasbage <amish.workspace@gmail.com>
2025-11-08 12:27:36 +0530

    Delete a Node.c

---
+++ Delete a Node.c
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+
+    if (position == 0) {
+        SinglyLinkedListNode* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+
+    SinglyLinkedListNode* current = head;
+    SinglyLinkedListNode* previous = NULL;
+
+   
+    for (int i = 0; i < position; i++) {
+        previous = current;
+        current = current->next;
+    }
+
+   
+    if (previous != NULL) {
+        previous->next = current->next;
+        free(current);
+    }
+    
+    return head;
+}
+
+
+int main()
